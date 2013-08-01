a4b26e87998cbfe281102430a27252c19b78f0b3
davidhhuan <davidhhuan@126.com>
2013-08-01 12:50:40 +0800

    Update .ssh_server.sh

--- shell/.ssh_server.sh
+++ shell/.ssh_server.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/expect -f
 set project [lindex $argv 0]
 switch $project {
-    dev {
-        set path shopnc@172.26.0.27
-        set password abc123!
-    }
-    qa {
-        set path shopnc@172.26.0.51
-        set password abc@2!1n
-    }
     event {
     	set path heng@event.shedunews.com
 	set password heng123456
